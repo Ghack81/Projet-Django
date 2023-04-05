4db9b90b9c888a3d658d2c901d41d5307a7910bb
Ghack81 <herve-mention@hotmail.com>
2023-04-05 17:30:55 +0200

    Add new file for article

--- Website/blog/urls.py
+++ Website/blog/urls.py
@@ -3,4 +3,5 @@ from .import views
 
 urlpatterns = [
     path('', views.blog_index, name='blog_index'),
+    path('article/<str:name/>', views.article, name='article'),
 ]
--- Website/blog/views.py
+++ Website/blog/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def blog_index(request):
     return render(request, 'blog/blog_index.html')
+
+def article(request, name):
+    return render(request, 'blog/article.html')
